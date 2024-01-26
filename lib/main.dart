73959b2d83825f2834fa49faf97062734887b423
MamtaBhatt885 <610mamtabhatt@gmail.com>
2024-01-26 07:32:11 +0545

    playlist provider is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:music_player/components/splash_screen.dart';
-import 'package:music_player/pages/home_page.dart';
+import 'package:music_player/models/playlist_provider.dart';
 import 'package:music_player/pages/theme_provider.dart';
-import 'package:music_player/themes/dark_mode.dart';
-import 'package:music_player/themes/light_mode.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => PlaylistProvider()),
+      ],
       child: const MyApp(),
     ),
   );
--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/models/song.dart';
 
-
-class PlaylistProvider extends ChangeNotifier{
+class PlaylistProvider extends ChangeNotifier {
   //playlist of songs
-final List<Song> _playlist =[
+  final List<Song> _playlist = [
+//song1
+    Song(
+        songName: "I see love",
+        artistName: "Jonas Blue",
+        albumArtImagePath: "assets/music_images/cover.jpg",
+        audioPath: "assets/music/audio.mpeg"),
 
-];
+//song2
+    Song(
+        songName: "Pehle Bhi Main",
+        artistName: "Raj Shekhar",
+        albumArtImagePath: "assets/music_images/music_cover.jpg",
+        audioPath: "assets/music/audio2.mpeg")
+ 
+ 
+  ];
+
+   //current song playing index
+   int? _currentSongIndex;
+
+   /*
+
+   Getters
+
+   */
+
+List<Song> get playlist => _playlist;
+int? get currentSongIndex => _currentSongIndex;
+
+
+  /*Setters*/
+set currentSongIndex(int? newIndex){
+  //update current song index
+  _currentSongIndex = newIndex;
+
+  //update UI
+  notifyListeners();
+}
+
+
 }
